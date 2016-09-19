dcd20cd58482a1a680b7f971e2403c143b2d1d70
FlameFractal <vishg8@gmail.com>
2016-09-19 22:35:21 +0530

    Modified server.js -local

--- server.js
+++ server.js
@@ -5,8 +5,39 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var article-one = {
+	title: "Article One";
+	content: `This is article oneeeeeeeeeeeeeeeeeeeeeeeeeee
+	<h1>ONE</h1>
+	lol
+	`;
+	style: "style.css";
+}
+
+
+funtion createPage(data){
+	var htmlTemplate = `
+	<html>
+		<head>
+			<title>${title}</title>
+		</head>
+		<body>
+			${content}
+		</body>
+	</html>
+	`;
+	return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article-one', function (req, res) {
+  res.send(createPage(article-one));
 });
 
 app.get('/ui/style.css', function (req, res) {

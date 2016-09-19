02bd8788ed81e2d18b4b36c4ea6242377d0f0dd4
FlameFractal <vishg8@gmail.com>
2016-09-19 23:26:42 +0530

    Modified server.js -local

--- server.js
+++ server.js
@@ -5,8 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+articles = {
+    'article-one': {
+    	title: 'Article One',
+    	content: 'This is article oneeeeeeeeeeeeeeeeeeeeeeeeeee<h1>ONE</h1>lol'
+    },
+    
+    'article-two': {
+    	title: 'Article Tqo',
+    	content: `
+    	This is article 
+    	<h1>TWO</h1>
+    	lol
+    	`
+    },
+    
+    'article-three': {
+    	title: 'Article Three',
+    	content: `
+    	This is article 
+    	<h1>THREE</h1>
+    	lol
+    	`
+    }
+};
+
+
+function createPage(data) {
+    
+    var title = data.title;
+    var content = data.content;
+    
+	var htmlTemplate = `
+	<html>
+		<head>
+			<title>${title}</title>
+		</head>
+		<body>
+			${content}
+		</body>
+	</html>
+	`;
+	return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+    res.send(createPage(articleName));
 });
 
 app.get('/ui/style.css', function (req, res) {

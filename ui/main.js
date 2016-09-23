960459c88e8845eceb95b48d23cce04f60b4f5cc
FlameFractal <vishg8@gmail.com>
2016-09-24 03:25:07 +0530

    Module P6 , AJAX counter

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+// var path = require('path');
+var button = document.getElementById('button');
+
+button.onclick = function(){
+	var req = new XMLHttpRequest();
+
+	req.onreadystatechange = function(){
+		if(req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+	            var count = req.responseText;
+	            var counter = document.getElementById('counter');
+	            counter.innerHTML = count.toString();
+	        }
+	};
+
+	req.open("GET", document.URL+"counter", true);
+	req.send(null);
+}

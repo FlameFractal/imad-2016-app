6e3c1ca7754d673b84aa113d5027dd0f5f90dbd8
FlameFractal <vishg8@gmail.com>
2016-09-24 00:21:23 +0530

    Module P6

--- server.js
+++ server.js
@@ -50,10 +50,15 @@ function createPage(data) {
 	return htmlTemplate;
 }
 
-
+var counter = 0;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/counter', function(req, res){
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 
 app.get('/:articleName', function (req, res) {

aa696d5106064f659c4902b8df067c298005dd91
FlameFractal <vishg8@gmail.com>
2016-11-07 15:32:53 +0530

    changed port for heroku deployment

--- server.js
+++ server.js
@@ -469,6 +469,6 @@ function postTemplate(data){
 
 
 var port = process.env.PORT || 8080;  // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });

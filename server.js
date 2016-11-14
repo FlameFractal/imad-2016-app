f8a87cb7bc87de6d238d6c366a16759c94e93a6a
FlameFractal <vishg8@gmail.com>
2016-11-14 14:44:40 +0530

    bugs

--- server.js
+++ server.js
@@ -424,13 +424,13 @@ function postTemplate(data){
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>${title}</title>
-            <link href="../css/post-comment.css" rel="stylesheet">
-            <link href="../css/modal.css" rel="stylesheet">
             <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
             <link href="../css/clean-blog.min.css" rel="stylesheet">
             <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
             <link href='//fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
             <link href='//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+            <link href="../css/post-comment.css" rel="stylesheet">
+            <link href="../css/modal.css" rel="stylesheet">
         </head>
         <body>
             <nav class="navbar navbar-default navbar-custom navbar-fixed-top">

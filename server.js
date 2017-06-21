3ab34d8de199c059fe8a79c7fdead48c70d2366e
Flame Fractal <vishg8@gmail.com>
2017-06-21 14:11:57 +0530

    fixing header image path

--- server.js
+++ server.js
@@ -616,7 +616,7 @@ function template404(postID){
                 </div>
             </div>
         </nav>
-        <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
+        <header class="intro-header" style="background-image: url('/img/home-bg.jpg')">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
